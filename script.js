9f9cae3dce73bef40847d1c480c2f5e8d3df9df7
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-11-21 12:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const student ={
+	name:"Abhi";
+}
+Object.prototype.getKeys = function(){
+	return Object.keys(this);
+};
+const keys = student.getKeys();
+console.log(keys);
